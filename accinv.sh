fab50a31965e15e9b27a087af3e07e62f59970a4
unknown <andyoolala@gmail.com>
2024-03-12 23:12:02 +0800

    20240312 commit

---
+++ accinv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+lslogins -aupc | cut -d: -f2 | sed 1d | while read ident ;do
+    echo -n "$ident,$(hostname -s),$(hostname -s),Linux Server,"
+    if [[ $(sudo -l -U $ident |grep '(ALL)'|wc -l) -gt 0 ]]
+    then
+        echo -n "Admin"
+    else
+        echo -n "Normal User"
+    fi
+    echo ",Active,$(date +"%Y/%m/%d")"
+done
