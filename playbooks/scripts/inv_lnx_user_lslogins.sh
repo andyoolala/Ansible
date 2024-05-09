21c12f2a5246371cec97fb25198d697e3a304a5e
unknown <andyoolala@gmail.com>
2024-05-09 23:31:32 +0800

    Changes to be committed: 	deleted:    playbooks/accountinv_rhel.yaml 	modified:   playbooks/ansible.cfg 	new file:   playbooks/inv_lnx_users.yaml 	new file:   playbooks/scripts/inv_lnx_ad_user.sh 	renamed:    playbooks/scripts/accinv.sh -> playbooks/scripts/inv_lnx_user_lslogins.sh 	new file:   playbooks/scripts/test.sh

--- playbooks/scripts/inv_lnx_user_lslogins.sh
+++ playbooks/scripts/inv_lnx_user_lslogins.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+## inventory local user using lslogins
+inv_local_user(){
 lslogins -aupc | cut -d: -f2 | sed 1d | while read ident ;do
     echo -n "$ident,$(hostname -s),$(hostname -s),Linux Server,"
-    if [[ $(sudo -l -U $ident |grep '(ALL)'|wc -l) -gt 0 ]]
+    if [[ $(sudo -l -U $ident |grep '(ALL) ALL'|wc -l) -gt 0 ]]
     then
         echo -n "Admin"
     else
@@ -10,3 +12,6 @@ lslogins -aupc | cut -d: -f2 | sed 1d | while read ident ;do
     fi
     echo ",Active,$(date +"%Y/%m/%d")"
 done
+}
+
+inv_local_user
