393e5bd18c4fd708bb5913275b17f3d51134e6f9
unknown <andyoolala@gmail.com>
2024-08-11 20:08:13 +0800

    modified:   playbooks/add_user.yaml 	modified:   playbooks/cm_invconf_lnx.yaml 	new file:   playbooks/cm_invserv_lnx_copy.yaml 	modified:   playbooks/cm_invspec_lnx.yaml 	modified:   playbooks/cm_invuser_lnx.yaml 	modified:   playbooks/scripts/inv_lnx_user_passwd.sh

--- playbooks/scripts/inv_lnx_user_passwd.sh
+++ playbooks/scripts/inv_lnx_user_passwd.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-## inventory local user using lslogins
+
 inv_local_user(){
-cat /etc/passwd |grep /bin/bash |cut -d: -f1 | while read ident ;do
+cat /etc/passwd  |cut -d: -f1 | while read ident ;do
     echo -n "$ident,$(hostname -s),$(hostname -s),Linux Server,"
     if [[ $(sudo -l -U $ident |grep 'ALL'|wc -l) -gt 0 ]]
-    then
-        echo -n "Admin"
-    else
-        echo -n "Normal User"
-    fi
-    echo ",Active,$(date +"%Y/%m/%d")"
+        then
+            echo -n "Admin,"
+        else
+            echo -n "User,"
+    fi 
+    case $(sudo passwd -S $ident | cut -d ' ' -f 2) in
+        PS)
+            echo -n "Active,"
+            ;;
+        LK)
+            echo -n "Locked,"
+            ;;
+        *)
+            echo -n "Undefined,"
+            ;;
+    esac
+    echo -n "$(sudo passwd -S $ident | cut -d ' ' -f 3),"
+    echo "$(date +"%Y-%m-%d")"
 done
 }
 
